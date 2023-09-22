152fefa3f706e5604b44f3f763a3e20e7ac1054a
onarman <onarmannn@gmail.com>
2023-09-22 22:52:19 +0200

    fixed a bug about loading

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import Movies from "@/mocks/movies.json";
 import { notFound } from "next/navigation";
 
-function MoviePage({ params, searchParams }) {
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function MoviePage({ params, searchParams }) {
+  await delay(2000);
   const movieDetail = Movies.results.find(
     (movie) => movie.id.toString() === params.id
   );
@@ -11,14 +16,12 @@ function MoviePage({ params, searchParams }) {
   if (!movieDetail) {
     notFound();
   }
-  if (searchParams?.error === "true") {
-    console.log("error uctu");
+  if (searchParams.error === "true") {
     throw new Error("Error happened");
   }
 
   return (
     <div>
-      {console.log(searchParams)}
       <MovieContainer movie={movieDetail} />
     </div>
   );
