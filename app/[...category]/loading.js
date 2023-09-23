fdee7001b0024a080eee8d91408cde07704f5cff
onarman <onarmannn@gmail.com>
2023-09-24 01:07:39 +0200

    loading and project done(with moc data)

---
+++ app/[...category]/loading.js
@@ -0,0 +1,16 @@
+import CategoriesLoading from "@/components/categories/loading";
+import FeatureMovieLoading  from "@/components/featured-movie/loading";
+import MoviesSectionLoading  from "@/components/movie-section/loading";
+import React from "react";
+
+function Loading() {
+  return <div>
+	<FeatureMovieLoading />
+	<CategoriesLoading />
+	<MoviesSectionLoading />
+	<MoviesSectionLoading />
+	<MoviesSectionLoading />
+  </div>;
+}
+
+export default Loading;
--- app/[...category]/page.jsx
+++ app/[...category]/page.jsx
@@ -2,7 +2,12 @@ import HomeContainer from "@/containers/home";
 import Movies from "@/mocks/movies.json";
 import React from "react";
 
-function HomePage({ params }) {
+async function delay(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+async function HomePage({ params }) {
+	await delay(2000)
   let selectedCategory;
   if (params.category.length > 0) {
     selectedCategory = {
