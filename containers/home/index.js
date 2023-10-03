eb6549926ffde1ae6dd276f4b1401452da58129f
onarman <onarmannn@gmail.com>
2023-10-03 18:58:22 +0200

    API activated and home page redesigned without mock

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,34 +1,32 @@
 import React from "react";
 
-import Movies from "@/mocks/movies.json";
-import Genres from "@/mocks/genres.json";
 import FeaturedMovie from "@/components/featured-movie";
 import Categories from "@/components/categories";
 import MovieSection from "@/components/movie-section";
 
-function HomeContainer({ selectedCategory }) {
+function HomeContainer({
+  trendMovies = [],
+  trendTv = [],
+  categories = [],
+  selectedCategory,
+}) {
   return (
     <div>
-      <FeaturedMovie movie={Movies.results[0]} />
-      <Categories categories={Genres.genres.slice(0, 5)} />
-      {selectedCategory && selectedCategory.movies.length > 0 && (
+      <FeaturedMovie movie={trendMovies?.[0]} />
+      <Categories categories={categories.slice(0, 5)} />
+      {selectedCategory.movies.length > 0 && (
         <MovieSection
           title={
-            Genres.genres.find((genre) => `${genre.id}` === selectedCategory.id)
-              .name
+            categories.find(
+              (category) => `${category.id}` === selectedCategory.id
+            )?.name || ""
           }
           movies={selectedCategory.movies}
         />
       )}
 
-      <MovieSection
-        title="Popular Movies"
-        movies={Movies.results.slice(1, 7)}
-      />
-      <MovieSection
-        title="Your Favourites"
-        movies={Movies.results.slice(7, 13)}
-      />
+      <MovieSection title="Trend Movies" movies={trendMovies.slice(1, 7)} />
+      <MovieSection title="Trend Tv" movies={trendTv.slice(7, 13)} />
     </div>
   );
 }
